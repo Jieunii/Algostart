ebed8bb38508ddf6820e80235273882c524d892c
dlfrnjs3456 <35252943+dlfrnjs3456@users.noreply.github.com>
2021-08-12 18:17:22 +0900

    Update 전화번호 목록.cpp

--- 최일권_문제풀이/전화번호 목록.cpp
+++ 최일권_문제풀이/전화번호 목록.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int main() {
-
-	vector<string> phone_book = { "119", "97674223", "1195524421" };
-	bool answer = true;
+bool solution(vector<string> phone_book) {
+    
+    bool answer = true;
 	sort(phone_book.begin(), phone_book.end());
 
-	for (int i = 0; i < phone_book.size(); i++)
-	{
-		cout << phone_book[i] << " ";
-	}
-	cout << endl;
-
-	return 0;
+	for (int i = 0; i < phone_book.size()-1; i++)
+	   if(phone_book[i+1].find(phone_book[i])==0)
+           return false;
+    return answer;
 }
