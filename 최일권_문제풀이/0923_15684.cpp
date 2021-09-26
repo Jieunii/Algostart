e3f609bfea6384f80aeda745b21281d00c506c5a
dlfrnjs3456 <35252943+dlfrnjs3456@users.noreply.github.com>
2021-09-26 23:38:30 +0900

    Update 0923_15684.cpp

--- 최일권_문제풀이/0923_15684.cpp
+++ 최일권_문제풀이/0923_15684.cpp
@@ -54,12 +54,6 @@ int main() {
 		ladder[b][a] = 1;
 	}
 
-	for (int i = 1; i <= H; i++) {
-		for (int j = 1; j <= N; j++) {
-			cout << ladder[i][j] << " ";
-		}
-		cout << endl;
-	}
 
 	set_ladder(0, 1);
 
