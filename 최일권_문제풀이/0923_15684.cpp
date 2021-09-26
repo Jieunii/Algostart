d3a9497fed8c3638de63c024bc91a6cc89f760a5
dlfrnjs3456 <35252943+dlfrnjs3456@users.noreply.github.com>
2021-09-26 23:47:27 +0900

    Update 0923_15684.cpp

--- 최일권_문제풀이/0923_15684.cpp
+++ 최일권_문제풀이/0923_15684.cpp
@@ -10,10 +10,10 @@ int test_ladder() {
 	for (int i = 1; i <= N; i++) {
 		int cur = i;
 		for (int j = 1; j <= H; j++) {
-			if (ladder[cur][j] == 1) cur += 1;
-			else if (ladder[cur - 1][j] == 1) cur -= 1;
+			if (ladder[cur][j] == 1) cur += 1; //현재 위치에 1 즉 사다리가 존재하면 오른쪽으로
+			else if (ladder[cur - 1][j] == 1) cur -= 1; //왼쪽에 존재하면 왼쪽으로
 		}
-		if (cur != i) return 0;
+		if (cur != i) return 0; //골 체크
 	}
 	return 1;
 }
@@ -31,7 +31,7 @@ void set_ladder(int start,int idx) {
 	for (int i = idx; i <= H; i++) {
 		for (int j = 1; j <= N; j++) {
 			if (ladder[j][i] == 1) continue;
-			if (ladder[j - 1][i] == 1) continue;
+			if (ladder[j - 1][i] == 1) continue;// 놓을려는 곳 옆에 사다리가 존재하면 반복하기 때문에 패스
 			if (ladder[j + 1][i] == 1) continue;
 			ladder[j][i] = 1;
 			set_ladder(start + 1, i);
@@ -51,14 +51,14 @@ int main() {
 		int a, b;
 		cin >> a >> b;
 
-		ladder[b][a] = 1;
+		ladder[b][a] = 1; //1(사다리)이 있을 경우 오른쪽으로 이동
 	}
 
 
 	set_ladder(0, 1);
 
 
-	if (ans > 3) cout << -1 << endl;
+	if (ans > 3) cout << -1 << endl; //3보다 크면 -1 출력
 	else
 		cout << ans << endl;
 	
